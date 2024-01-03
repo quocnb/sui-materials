b0b0aa3a0376c3697cc79232f02f92671c73edf7
Quoc Nguyen <quocnb.vnu@gmail.com>
2024-01-04 07:37:24 +0900

    Chapter 23: Converting an ios app to macos

---
+++ 23-converting-an-ios-app-to-macos/projects/MountainAirportMac/MountainAirportMac/MountainAirportMacApp.swift
@@ -0,0 +1,18 @@
+//
+//  MountainAirportMacApp.swift
+//  MountainAirportMac
+//
+//  Created by Quoc Nguyen on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct MountainAirportMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+						.frame(minWidth: 700)
+        }
+    }
+}
